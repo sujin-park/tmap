737d68ed112f4bc677d8a60fbc918d6afb7a40ff
sujin <sujin@sujin>
2017-06-26 14:31:30 +0900

    review controller 통합 commit

--- secondproject/src/com/secondproject/review/controller/ReviewController.java
+++ secondproject/src/com/secondproject/review/controller/ReviewController.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.secondproject.factory.ReviewFactory;
-import com.secondproject.factory.ShopFactory;
+import com.secondproject.factory.*;
+import com.secondproject.util.Encoding;
 import com.secondproject.util.PageMove;
 
 @WebServlet("/review")
@@ -17,9 +17,16 @@ public class ReviewController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String act = request.getParameter("act");
-		
+		String path = "/index.jsp";
+		String contentPath = "";
+		String key = Encoding.nullToBlank(request.getParameter("key"));
+		String word = request.getParameter("word");
+		String order = Encoding.nullToBlank(request.getParameter("order"));
+		String column = Encoding.nullToBlank(request.getParameter("column"));
+		String queryString = "?key=" + key + "&word=" + Encoding.urlFormat(word) + "&order=" + order + "&column="
+				+ column;
 		if ("writeForm".equals(act)) {
-			String contentPath = "/page/review/write.jsp";
+			contentPath = "/page/review/write.jsp";
 			request.setAttribute("titleTagValue", "TMAP - 리뷰작성");
 			request.setAttribute("contentPath", contentPath);
 			request.setAttribute("addHeadPath", "/page/review/include/head.jsp");
@@ -27,8 +34,24 @@ public class ReviewController extends HttpServlet {
 			PageMove.forward("/template/default/default.jsp", request, response);
 		} else if ("write".equals(act)) {
 			ReviewFactory.getReviewWriteAction().execute(request, response);
+		} else if ("view".equals(act)) {
+
+			contentPath = AdminFactory.getReviewViewAction().execute(request, response);
+			path = "/template/admin/admin.jsp";
+			request.setAttribute("titleTagValue", "타이틀");
+			request.setAttribute("contentPath", contentPath);
+			request.setAttribute("addHeadPath", "/template/admin/include/head.jsp");
+			request.setAttribute("addBottomPath", "/page/adminpage/include/bottom_exhibition.jsp");
+
+			PageMove.forward(path, request, response);
+		} else if ("blind".equals(act)) {
+			contentPath = AdminFactory.getReviewBlindAction().execute(request, response);
+			String url = "/admin?act=mvreview";
+
+			PageMove.redirect(url, request, response);
 		}
 	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
