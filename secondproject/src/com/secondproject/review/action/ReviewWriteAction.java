1370cf27386c5431f2b17e9f186827f64affef02
HK <HK@HK9560>
2017-06-27 09:22:13 +0900

    ㅇㅇㅇ?

--- secondproject/src/com/secondproject/review/action/ReviewWriteAction.java
+++ secondproject/src/com/secondproject/review/action/ReviewWriteAction.java
@@ -15,40 +15,27 @@ public class ReviewWriteAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		boolean isValidate = false;
-		
+		String path = "";
 		int shopId = NumberCheck.nullToZero(request.getParameter("shopId"));
-		int userId = NumberCheck.nullToZero(request.getParameter("userId"));
+		//int userId = NumberCheck.nullToZero(request.getParameter("userId"));
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 		int score = Integer.parseInt(request.getParameter("score"));
-
-		if (shopId == 0) {
-			
-		} else if (userId == 0) {
-			
-		}
-			
-		
-
-		
-		// shopId == 0 || userId == 0 이라면 서비스에 일 시킬필요가 없음.
-		// 그렇다면.....실패페이지로 바로 가버리면 될테고..
-		// 실패페이지에서는 경고창만 뛰우고 뒤로가기?
 		
 		ReviewDto reviewDto = new ReviewDto();
 		reviewDto.setShopId(shopId);
-		reviewDto.setUserId(userId);
+		reviewDto.setUserId(1);
 		reviewDto.setTitle(title);
 		reviewDto.setContent(content);
 		reviewDto.setScore(score);
-		ReviewServiceImple.getReviewService().addReview(reviewDto);
+		int isSuccess = ReviewServiceImple.getReviewService().addReview(reviewDto);
+		if (isSuccess == 1) {
+			path = "/review?act=view&reviewId=" + 1;
+		} else {
+			
+		}
 		
-		return null;
-	}
-	
-	private boolean validate() {
-		return false;
+		return path;
 	}
 
 }
