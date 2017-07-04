25b02019e162cccd740a8612afc5b093c1080509
homin <homin@DESKTOP-NF40GOK>
2017-07-04 14:46:07 +0900

    aaa

---
+++ secondproject/src/com/secondproject/mypage/action/FollowSelect.java
@@ -0,0 +1,20 @@
+package com.secondproject.mypage.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.secondproject.action.Action;
+
+public class FollowSelect implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
